08f5a0f26ed0099fb192281289e73038bad3faef
MinhTruong <mtruong2002@gmail.com>
2023-01-02 16:16:45 +0700

    Show input AdEditUser

--- front-end/src/features/admin/components/AdEditUser.js
+++ front-end/src/features/admin/components/AdEditUser.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useState } from 'react';
 import { Segment } from 'semantic-ui-react'
+import { useNavigate } from 'react-router-dom';
+
 import './AdEditUser.css'
 
 export default function AdEditUser() {
+  const [password, setPassword] = useState('');
+  const [isActive, setIsActive] = useState();
+  const [role, setRole] = useState();
+  const navigate = useNavigate()
+
+  const handleClickApply = () => {
+    console.log(password)
+    console.log(role)
+    console.log(isActive)
+  };
+
+  const handleClickCancle = () => navigate('/admin')
+  
   return (
     <>
     <div className="framebox">
@@ -23,12 +39,12 @@ export default function AdEditUser() {
 
       <div className="input-group mb-3">
         <label for="inputPassword5" className="input-group-text">Password</label>
-        <input type="password" id="inputPassword5" className="form-control" aria-describedby="passwordHelpBlock"/>
+        <input type="password" id="inputPassword5" className="form-control" aria-describedby="passwordHelpBlock" value={password} onChange={(e)=>setPassword(e.target.value)}/>
       </div>
 
       <div className="input-group mb-3">
         <span className="input-group-text" id="basic-addon3">Role</span>
-          <select className="ui dropdown">
+          <select className="ui dropdown" value={role} onChange={(e)=>setRole(e.target.value)}>
             <option>Sub-admin</option>
             <option>User</option>
           </select>
@@ -36,7 +52,7 @@ export default function AdEditUser() {
 
       <div className="input-group mb-3">
         <span className="input-group-text" id="basic-addon4">Active</span>
-          <select className="ui dropdown">
+          <select className="ui dropdown" value={isActive} onChange={(e)=>setIsActive(e.target.value)}>
             <option>Yes</option>
             <option>No</option>
           </select>
@@ -51,7 +67,12 @@ export default function AdEditUser() {
         <span className="input-group-text" id="basic-addon6">Last Login</span>
         <span className="form-control" id="basic-infor4">26 min ago</span>
       </div>
-      
+
+      <div className='btnn row'>
+          <button type="submit" className="btn btn-success" onClick={handleClickApply}>Apply</button>
+          <button type="submit" className="btn btn-danger" onClick={handleClickCancle}>Cancle</button>
+      </div>
+
     </Segment>
     </div>
     </>
