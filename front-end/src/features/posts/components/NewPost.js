94ffab6fb36275a064e1f0083faefba9ba31ab72
Mai Trần Khánh Duy <maitkduy@gmail.com>
2023-01-07 02:01:10 +0700

    Thêm Link anh file NewPost.js

--- front-end/src/features/posts/components/NewPost.js
+++ front-end/src/features/posts/components/NewPost.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState,useRef } from 'react'
 import {useNavigate,useParams} from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { setCredentials } from 'features/auth/authSlice'
@@ -21,8 +21,14 @@ export default function NewPost() {
     const {token} = useSelector(state=> state.auth)
     const {id} = useParams()
     const {username} = useParams()
+    const link1 = useRef()
+    const link2 = useRef()
+    const link3 = useRef()
+    const link4 = useRef()
+    const link5 = useRef()
     const handleSubmit = event =>{
-    event.preventDefault()  
+    event.preventDefault()
+    const imageURL = [link1.current.value,link2.current.value,link3.current.value,link4.current.value,link5.current.value];  
     const newPost = {
         nameProduct: name,
         describePost: desc,
@@ -30,7 +36,7 @@ export default function NewPost() {
         priceProduct: price,
         amountRegistry: quantity,
         typeProduct: type,
-        imageURL: linkAnh
+        imageURL: imageURL
     }
     const config = {
         headers: {
@@ -72,7 +78,7 @@ export default function NewPost() {
   return (
     <>
     <form onSubmit={handleSubmit}>
-        <Segment style = {{backgroundColor: "#A0B4F3"}} className = "outlineFrame">
+        <div className = "outlineFrame">
             <div as = 'h1' className = "text_themsanpham">
                     THÊM 1 SẢN PHẨM MỚI
             </div>
@@ -83,7 +89,7 @@ export default function NewPost() {
                 <div className="form-group row">
                     <label htmlFor="productName" className="col-sm-2 col-form-label">Tên sản phẩm</label>
                     <div className="col-sm-10">
-                        <input type="text" className="form-control" id="tensp" value={name} onChange={(e)=>setName(e.target.value)}  placeholder="Nhập tên sản phẩm"/>
+                        <input required type="text" className="form-control" id="tensp" value={name} onChange={(e)=>setName(e.target.value)}  placeholder="Nhập tên sản phẩm"/>
                     </div>
                 </div>
                 <br/>
@@ -93,12 +99,10 @@ export default function NewPost() {
                         <input type="text" className="form-control" id="motasp" value={desc} onChange={(e)=>setDesc(e.target.value)} placeholder="Nhập mô tả sản phẩm"/>
                     </div>
                 </div> */}
-                <div className="ui form">
-                    <div className="field">
-                        <label>Mô tả sản phẩm</label>
-                        <textarea placeholder='Vui lòng nhập mô tả sản phẩm' className="form-control" id="motasp" value={desc} onChange={(e)=>setDesc(e.target.value)}>
-                        </textarea>
-                    </div>
+                <div className="field">
+                    <label>Mô tả sản phẩm</label>
+                    <textarea placeholder='Vui lòng nhập mô tả sản phẩm' className="form-control" id="motasp" value={desc} onChange={(e)=>setDesc(e.target.value)}>
+                    </textarea>
                 </div>
                 <br/>
                 <div className="form-group row">
@@ -111,21 +115,21 @@ export default function NewPost() {
                 <div className="form-group row">
                     <label htmlFor="productName" className="col-sm-2 col-form-label">Giá mặt hàng (Tối đa: 150.000VNĐ)</label>
                     <div className="col-sm-10">
-                        <input type="number" className="form-control" id="giatien" maxlength="6" value={price} onChange={(e)=>setPrice(e.target.value)}  placeholder="Nhập số tiền (VNĐ)"/>
+                        <input required type="number" className="form-control" id="giatien" maxlength="6" value={price} onChange={(e)=>setPrice(e.target.value)}  placeholder="Nhập số tiền (VNĐ)"/>
                     </div>
                 </div>
                 <br/>
                 <div className="form-group row">
                     <label htmlFor="productName" className="col-sm-2 col-form-label">Số lượng</label>
                     <div className="col-sm-10">
-                        <input type="number" className="form-control" id="soluong" value={quantity} onChange={(e)=>setQuantity(e.target.value)}  placeholder="Nhập số lượng sản phẩm"/>
+                        <input required type="number" className="form-control" id="soluong" value={quantity} onChange={(e)=>setQuantity(e.target.value)}  placeholder="Nhập số lượng sản phẩm"/>
                     </div>
                 </div>
                 <br/>
                 <div className="form-group row">
                     <label htmlFor="productName" className="col-sm-2 col-form-label">Loại hàng hóa</label>
                     <div className="col-sm-10">
-                        <input type="text" className="form-control" id="loaihanghoa" value={type} onChange={(e)=>setType(e.target.value)}  placeholder="Nhập loại hàng hóa"/>
+                        <input required type="text" className="form-control" id="loaihanghoa" value={type} onChange={(e)=>setType(e.target.value)}  placeholder="Nhập loại hàng hóa"/>
                     </div>
                 </div>
             </Segment>
@@ -136,11 +140,11 @@ export default function NewPost() {
                 <div className="form-group row">
                     <label htmlFor="productImage" className="col-sm-2 col-form-label">Link ảnh sản phẩm</label>
                     <div className="col-sm-10">
-                        <input type="text" className="form-control" id="linkanh1" value={linkAnh} onChange={(e)=>setLinkAnh(e.target.value)}  placeholder="Thêm Link ảnh sản phẩm 1"/>
-                        <input type="text" className="form-control" id="linkanh2" value={linkAnh} onChange={(e)=>setLinkAnh(e.target.value)}  placeholder="Thêm Link ảnh sản phẩm 2"/>
-                        <input type="text" className="form-control" id="linkanh3" value={linkAnh} onChange={(e)=>setLinkAnh(e.target.value)}  placeholder="Thêm Link ảnh sản phẩm 3"/>
-                        <input type="text" className="form-control" id="linkanh4" value={linkAnh} onChange={(e)=>setLinkAnh(e.target.value)}  placeholder="Thêm Link ảnh sản phẩm 4"/>
-                        <input type="text" className="form-control" id="linkanh5" value={linkAnh} onChange={(e)=>setLinkAnh(e.target.value)}  placeholder="Thêm Link ảnh sản phẩm 5"/>
+                        <input ref={link1} type="text" className="form-control" id="linkanh1" onChange={(e)=>setLinkAnh(e.target.value)}  placeholder="Thêm Link ảnh sản phẩm 1"/>
+                        <input ref={link2} type="text" className="form-control" id="linkanh2" onChange={(e)=>setLinkAnh(e.target.value)}  placeholder="Thêm Link ảnh sản phẩm 2"/>
+                        <input ref={link3} type="text" className="form-control" id="linkanh3" onChange={(e)=>setLinkAnh(e.target.value)}  placeholder="Thêm Link ảnh sản phẩm 3"/>
+                        <input ref={link4} type="text" className="form-control" id="linkanh4" onChange={(e)=>setLinkAnh(e.target.value)}  placeholder="Thêm Link ảnh sản phẩm 4"/>
+                        <input ref={link5} type="text" className="form-control" id="linkanh5" onChange={(e)=>setLinkAnh(e.target.value)}  placeholder="Thêm Link ảnh sản phẩm 5"/>
                     </div>
                 </div>
             </Segment>
@@ -150,19 +154,35 @@ export default function NewPost() {
                 </div>
                 <div className="form-group row">
                     <label htmlFor="productName" className="col-sm-2 col-form-label">Nhập câu hỏi cho người nhận/mua</label>
-                    <div className="col-sm-10">
-                        <input type="text" className="form-control" id="question" value={question} onChange={(e)=>setQuestion(e.target.value)}  placeholder="Vui lòng điền câu hỏi"/>
+                    <div className="ui disabled input">
+                        <input type="text" className="form-control" id="question" value={question} onChange={(e)=>setQuestion(e.target.value)}  placeholder="Why do you need the item?:"/>
+                    </div>
+                    <label htmlFor="productName" className="col-sm-2 col-form-label">Nhập câu hỏi cho người nhận/mua</label>
+                    <div className="ui disabled input">
+                        <input type="text" className="form-control" id="question" value={question} onChange={(e)=>setQuestion(e.target.value)}  placeholder="Do you need it right now?"/>
+                    </div>
+                    <label htmlFor="productName" className="col-sm-2 col-form-label">Nhập câu hỏi cho người nhận/mua</label>
+                    <div className="ui disabled input">
+                        <input type="text" className="form-control" id="question" value={question} onChange={(e)=>setQuestion(e.target.value)}  placeholder="What is your degree?"/>
+                    </div>
+                    <label htmlFor="productName" className="col-sm-2 col-form-label">Nhập câu hỏi cho người nhận/mua</label>
+                    <div className="ui disabled input">
+                        <input type="text" className="form-control" id="question" value={question} onChange={(e)=>setQuestion(e.target.value)}  placeholder="New publisher of LOL video game?"/>
+                    </div>
+                    <label htmlFor="productName" className="col-sm-2 col-form-label">Nhập câu hỏi cho người nhận/mua</label>
+                    <div className="ui disabled input">
+                        <input type="text" className="form-control" id="question" value={question} onChange={(e)=>setQuestion(e.target.value)}  placeholder="Can you please rate 5 stars for my shop?"/>
                     </div>
                 </div>
                 <hr/>
-                <a className="ui red tag label">Thêm câu hỏi</a>
+                {/* <a className="ui red tag label">Thêm câu hỏi</a> */}
             </Segment>
             <div className="ui buttons">
                 <button className="ui button" onClick = {navigateHome}>Hủy</button>
                 <div className="or"></div>
                 <button className="ui positive button">Thêm sản phẩm </button>
             </div>
-        </Segment>
+        </div>
 
     </form>
     </>
