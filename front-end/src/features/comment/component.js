ecec60de64cecba1573fc9c17ebf1bd9de8791d8
MinhTruong <mtruong2002@gmail.com>
2023-01-06 22:23:54 +0700

    fix order

--- front-end/src/features/comment/component.js
+++ front-end/src/features/comment/component.js
@@ -1,18 +1,20 @@
 import axios from 'axios'
 import useAuth from 'hooks/useAuth'
 import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import './component.css'
 import 'react-rater/lib/react-rater.css'
 import Rater from 'react-rater'
+import { findUserbyObject } from 'features/users/userSlice'
 
 
 export default function Comments(props) {
   const [nameUser,setNameUser] = useState()
   const [listComment,setListComment] = useState([])
   const id = props.id
-
-
+  const dispatch = useDispatch();
+  const userlist = useSelector(state=>state.userList.listUserofObject)
+  console.log(userlist)
 
   useEffect(()=>{
     const config = {
@@ -20,10 +22,10 @@ export default function Comments(props) {
         "Content-Type": "application/json"
       },
     };
-  axios.get(`http://localhost:5000/postss/${id}/comment`,config).then((res)=>{
+  axios.get(`http://localhost:5000/posts/${id}/comment`,config).then((res)=>{
       console.log(res.data)
       setListComment(res.data)
-
+      dispatch(findUserbyObject(res.data))
       return res.data;
       
   }).catch((err)=>{
